bf0bec3ecb17f58cd872adb4239f53c8aa06896e
olivi <ojryan01@gmail.com>
2021-10-06 09:27:56 -0400

    initial commit

---
+++ HeartBeatCalculator/Patient.cs
@@ -0,0 +1,120 @@
+﻿using Spectre.Console;
+using System;
+
+namespace cardiacanalyzer
+{
+    public class patient //classes are private by default so we need to make them publically accessible
+    {
+
+        //a constructor is a special kind of method, named with the class's name, executed each time an instance of the class is execute.
+        //use ctor tab tab
+
+        public patient()
+        {
+
+        }
+
+        public patient(int PatientId)
+        {
+            PatientId = patientId;
+        }
+
+        //long hand example of encapsulation shown below:
+
+        private string firstName; //data is encapsulated or hidden within the classes using a private backing field
+
+        public string FirstName //the getter (gets property value) / setter (assigns value to backing field) makes the backing field accessible through the property
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+
+        }
+
+        //for the rest of the properties, we will use short hand encapsulation, where the backing field is set up autom-implemented behind the scenes 
+
+
+        public string LastName { get; set; }
+
+        private int patientId;
+        public int PatientId { get; private set; }
+
+        public string FullName
+        {
+            get
+            {
+                return LastName + ' ' + FirstName;
+            }
+        }
+
+        //DateType DateOfBirth { get; set; }
+
+        //string AnalyzeECG() { get; set; } //calculate heart rate and return if the patient is healthy, has bradycardia or tachycardia
+
+        [Obsolete]
+        public static void PlotECG() //when we create an object instance then it doesn't need to be static anymore?
+
+        {
+            var canvas = new Canvas(16, 16);
+
+            // Draw some shapes
+            for (var i = 0; i < canvas.Width; i++)
+            {
+                // Cross
+                canvas.SetPixel(i, i, Color.White);
+                canvas.SetPixel(canvas.Width - i - 1, i, Color.White);
+
+                // Border
+                canvas.SetPixel(i, 0, Color.Red);
+                canvas.SetPixel(0, i, Color.Green);
+                canvas.SetPixel(i, canvas.Height - 1, Color.Blue);
+                canvas.SetPixel(canvas.Width - 1, i, Color.Yellow);
+            }
+
+            AnsiConsole.Render(canvas);
+            Console.ReadLine();
+
+        }
+
+        // Render the canvas
+
+        //int calculateHeartRate() { get; set; }
+
+        //Display patient data by id
+
+        //public Patient Retrieve(int PatientId)
+        //{
+        //    //code that retrieves
+        //    return new Patient();
+        //}
+
+        ////Display all patients
+
+        //public List<Patient> Retrieve()
+        //{
+        //    //code that retrieves all customers
+        //    return new List<Patient>();
+        //}
+
+
+        //Validates patient data
+        public bool Validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(FirstName)) isValid = false;
+            return isValid;
+
+        }
+
+        //importECG(); //import a data set for the selected patient?
+
+        // we want to initialize an empty array that will populate with the ECG data which can then be analyzed for that patient double ecgData[] {get; set; } 
+
+    }
+}
---
+++ HeartBeatCalculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using Spectre.Console;
+using System;
+
+namespace cardiacanalyzer
+{
+    class Program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("****************************");
+            Console.WriteLine("Welcome to Cardiac Analyzer!");
+            Console.WriteLine("****************************");
+
+            Console.WriteLine("Choose an option to get started:");
+            Console.WriteLine("--------------------------------");
+
+            string userSelection;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("1. Import ECG Data");
+            Console.WriteLine("2. Calculate heart rate");
+            Console.WriteLine("3. Graph ECG");
+            Console.WriteLine("4. Create new Patient");
+
+            userSelection = Console.ReadLine();
+
+            switch (userSelection)
+            {
+                case "1":
+                    Console.WriteLine("Reading ECG data...");
+                    break;//Load ECG data from a csv file
+                case "2":
+                    Console.WriteLine("Calculating heart rate...");
+                    break; //determine the heart rate by detecting the number of peaks over time and display a diagnosis
+                case "3":
+                    Console.WriteLine("Graphing ECG data...");
+                    patient.PlotECG();
+                    break; //graphically display the data
+                case "4":
+                    Console.WriteLine("Enter First Name:");
+                    break;
+
+            }
+
+        }
+    }
+}
