2ab231926ab9706c6cc2cf7155f97516e7966e16
olivi <ojryan01@gmail.com>
2021-10-18 12:21:34 -0400

    finished csv read and convert to double. simple frequency detection for sine wave

--- HeartBeatCalculator/EKGStudy.cs
+++ HeartBeatCalculator/EKGStudy.cs
@@ -14,13 +14,13 @@ namespace HeartBeatCalculator
     {
         public int StudyID { get; }
 
-        public string PatientName { get;  }
+        public string PatientName { get; }
 
-        public int Duration { get;  }
+        public int Duration { get; }
 
-        public List<int> StudyData { get;  }
+        public List<float> StudyData { get; }
 
-        public EKGStudy(int studyID, string patientName, int duration, List<string> studyData)
+        public EKGStudy(int studyID, string patientName, int duration, List<float> studyData)
 
         {
             this.StudyID = studyID;
@@ -30,60 +30,108 @@ namespace HeartBeatCalculator
 
 
         //import data into list from CSV
-        public static List<string> ReadEKG()
+        public static List<float> ReadEKG()
         {
 
+            //user enters file path
+            
             Console.WriteLine("Enter the file path:");
 
             string path = Console.ReadLine();
 
+            //user enters the sampling frequency in hertz
+            
+            Console.WriteLine("Enter the sample frequency in hertz:");
+          
+            string frequencyString = Console.ReadLine();
+
+            int frequency = Int32.Parse(frequencyString);
+
             Console.WriteLine("Reading ECG data...");
 
-            List<string> studyData = File.ReadAllLines(path).ToList(); //read all lines only has a type of string. need to parse this into int values
+            //logic to read the csv into a list of strings
             
-            foreach(string i in studyData)
-            { 
-            Console.WriteLine(i);
-            }
+            var reader = new StreamReader(File.OpenRead(path)); //read the file
+
+            List<string> studyDataString = new List<string>();
+
+            while (!reader.EndOfStream) //until we get to end of file
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    foreach (var item in values)
+                    {
+                        studyDataString.Add(item);
+                    }
+                }
+
+            List<float> studyData = studyDataString.Select( x => float.Parse(x)).ToList(); // Convert list of strings to list of floats
+
+            double duration = studyDataString.Count / frequency; // duration in seconds
+
+            int pulseCounter = 0;
+
+            foreach (float i in studyData)
+             {
+
+                if (i == 0) //simple zero crossing detection for a pur sine wave (going to need different algo for EKG
+                    {
+                    pulseCounter += 1;   
+                    } 
+
+                Console.WriteLine(i);
+                
+             }
+
+            var heartRate = 60 * pulseCounter / duration; //heartrate in beats per min
+
+            Console.WriteLine($"The study contains {studyDataString.Count} data points"); //verifying that all the data points got read into list
+
+            Console.WriteLine($"The study duration is { duration } seconds"); //verifying that all the data points got read into list
+
+            Console.WriteLine($"The average heart rate is { heartRate } beats per minute");
+
             return studyData;
         }
+        
 
-       
-        [Obsolete]
 
-        public static void PlotECG() //when we create an object instance then it doesn't need to be static anymore?
+            [Obsolete]
 
-        {
-            var canvas = new Canvas(300, 300);
+            public static void PlotECG() //when we create an object instance then it doesn't need to be static anymore?
 
-            // Draw some shapes
-            for (var i = 0; i < canvas.Width; i++)
             {
+                var canvas = new Canvas(300, 300);
+
+                // Draw some shapes
+                for (var i = 0; i < canvas.Width; i++)
+                {
 
 
-                var plotx = i;
-                var ploty = Math.Ceiling(50 + 50 * Math.Sin(i * (Math.PI / 180)));
-                Console.WriteLine($"{plotx}, {ploty}");
+                    var plotx = i;
+                    var ploty = Math.Ceiling(50 + 50 * Math.Sin(i * (Math.PI / 180)));
+                    Console.WriteLine($"{plotx}, {ploty}");
 
-                //straight line
-                canvas.SetPixel(plotx, (int)ploty, Color.Blue);
+                    //straight line
+                    canvas.SetPixel(plotx, (int)ploty, Color.Blue);
 
-                // Cross
-                //canvas.SetPixel(i, i, Color.White);
-                //canvas.SetPixel(canvas.Width - i - 1, i, Color.White);
+                    // Cross
+                    //canvas.SetPixel(i, i, Color.White);
+                    //canvas.SetPixel(canvas.Width - i - 1, i, Color.White);
 
-                // Border
-                canvas.SetPixel(i, 0, Color.Red);
-                canvas.SetPixel(0, i, Color.Green);
-                canvas.SetPixel(i, canvas.Height - 1, Color.Blue);
-                canvas.SetPixel(canvas.Width - 1, i, Color.Yellow);
+                    // Border
+                    canvas.SetPixel(i, 0, Color.Red);
+                    canvas.SetPixel(0, i, Color.Green);
+                    canvas.SetPixel(i, canvas.Height - 1, Color.Blue);
+                    canvas.SetPixel(canvas.Width - 1, i, Color.Yellow);
+                }
+
+                AnsiConsole.Render(canvas);
+                Console.ReadLine();
+
             }
 
-            AnsiConsole.Render(canvas);
-            Console.ReadLine();
-
         }
-
     }
-}
+
 
